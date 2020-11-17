accba0308c88525c8afe4fbb7bf51b2bb81dfb56
Miguel Camacho <miguel.camacho-rufinon@ens-lyon.fr>
2020-11-17 14:27:24 +0000

    Script: fastqc.sh This script was made in order to make the quality control of the 6 RNA-seq files.

--- Control_quality.sh
+++ Control_quality.sh
@@ -1,18 +1,20 @@
+# Create a working directory:
+data="/home/rstudio/data/mydatalocal/data"
+mkdir -p $data
+cd $data
+
 #Create a working directory for your Control analysis
 
 mkdir -p sra_data_quality
 
-
-
 # For each SRR accession, do a control quality of eahc file in sra_data folder :
-``
+
 sra_data="/home/rstudio/data/mydatalocal/data/sra_data"
 fastq=$sra_data/*.fastq
 
 
 for var_a in $fastq
 do
-
 
 fastqc -o sra_data_quality -t 16 $var_a
 done

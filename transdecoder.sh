2b41edf5f4eb70e73e0aa12962a6d25d69d40a68
Miguel <you@example.com>
2020-12-15 15:52:19 +0000

    On a lance le scritp transdecoder

--- transdecoder.sh
+++ transdecoder.sh
@@ -2,13 +2,14 @@
 
 # Set a working directory:
 data="/home/rstudio/data/mydatalocal/data"
-mkdir -p $data
 cd $data
 
 
-mkdir -p data_transcod
+mkdir -p data_transcoder
 # Step 1: extract the long open reading frames
-TransDecoder.LongOrfs -t $data/sra_data_Trinity/Trinity.fasta -m ???
+TransDecoder.LongOrfs -t $data/data_trinity/Trinity.fasta --gene_trans_map $data/data_trinity/Trinity.fasta.gene_trans_map -m 100 -S -O data_transcoder
+
 # Step 2: (facultative) identify ORFs with homology to known proteins via blast or pfam searches.
+
 # Step 3: predict the likely coding regions
-TransDecoder.Predict -t $data/sra_data_Trinity/Trinity.fasta [ homology options ] --output_dir $data/data_transcod
+TransDecoder.Predict -t $data/data_trinity/Trinity.fasta --single_best_only --output_dir $data/data_transcoder
